3e85ab7b1f2fffa3cf4cb17201270158a61d3a41
Thariq21 <thariqrahman05@gmail.com>
2024-11-08 11:16:29 +0700

    Test

---
+++ app.js
@@ -0,0 +1,7 @@
+const a = 2;
+const b = 3;
+let c = a + b;
+
+console.log(c);
+
+console.log("Baptista Yohana Vallen Pacar Toriq");
---
+++ test.py
@@ -0,0 +1,11 @@
+my_idol = {
+        "nama": "",
+        "umur": 0,
+        "tanggal lahir": "",
+        }
+
+my_idol["nama"] = input("masukan nama: ")
+my_idol["umur"] = int(input("masukan umur: "))
+my_idol["tanggal lahir"] = input("masukan tanggal lahir: ")
+
+print(my_idol)
